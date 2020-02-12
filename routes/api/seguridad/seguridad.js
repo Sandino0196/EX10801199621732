011448913565f3562584e1482c1212ef2b132d03
Rodrigo Sandino <sandinorodrigo@gmail.com>
2020-02-11 21:10:27 -0600

    Metodo Post

--- routes/api/seguridad/seguridad.js
+++ routes/api/seguridad/seguridad.js
@@ -4,11 +4,18 @@ var userModel = require('./seguridad.model');
 
 router.get('/users/all', (req, res)=>{
     return res.status(200).json(imgModel.getAll());
-} ); // get users/all
+} );
 
-// http://localhost:3000/api/seguridad/users/1
 router.get('/users/:id',(req, res)=>{
     var id = parseInt( req.params.id );
     var img = imgModel.getById(id);
     return res.status(200).json(user);
 });
+
+router.post('/users/new', (req, res)=>{
+  var datosEnviados = req.body;
+  var newImg = imgModel.addNew(datosEnviados);
+  return res.status(200).json(newImg);
+});
+
+module.exports = router;
--- routes/api/seguridad/seguridad.model.js
+++ routes/api/seguridad/seguridad.model.js
@@ -3,13 +3,29 @@ var fileToSave = 'security.json';
 var imgModel = {};
 var imgCollection = [];
 
+function writeToFile(){
+  var serializedJSON = JSON.stringify(imgCollection);
+  fs.writeFileSync(fileToSave, serializedJSON, { encoding: 'utf8' } );
+  return true;
+}
+function openFile(){
+  try{
+  var serializedJSON = fs.readFileSync(fileToSave,{encoding:'utf8'});
+  imgCollection = JSON.parse(serializedJSON);
+  } catch(e){
+    console.log(e);
+  }
+}
+
 var imgTemplate = {
   imgID:'',
   imgTitle:"",
   imgUrlOriginal:"",
   imgUrlPreview:"",
-  imbAlbum: ""
+  imgAlbum: ""
 }
+
+openFile();
 
 imgModel.getAll = ()=>{
   return imgCollection;
@@ -28,6 +44,22 @@ imgModel.getById = (id)=>{
   }
 }
 
+imgModel.addNew = ({ imgtitle, imgurloriginal, imgurlpreview, imgalbum }  )=>{
+  var newImg = Object.assign(
+    {},
+    imgTemplate,
+    {
+      imgTitle: imgtitle,
+      imgUrlOriginal: imgurloriginal,
+      imgUrlPreview: imgurlpreview,
+      imgAlbum: imgalbum
+    }
+  );
+  newImg.userID = imgCollection.length + 1;
 
+  imgCollection.push(newImg);
+  writeToFile();
+  return newImg;
+}
 
 module.exports = imgModel;
